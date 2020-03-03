1fa96930626870ea41e6286c00f02a68ae67edd0
matmat-code <kidosol5053@gmail.com>
2020-03-03 19:40:28 +0800

    lets go man

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 varGameState = {
     preload: function(){
         this.load.image('background', 'assets/images/background.png');
+        this.load.image('chicken', 'assets/images/chicken.png');
+        this.load.image('horse', 'assets/images/horse.png');
+        this.load.image('pig', 'assets/images/pig.png');
+        this.load.image('sheep', 'assets/images/sheep3.png');
+        
     },
     create: function(){
         this.background = this.game.add.sprite(0,0,'background');
+        this.chicken = this.game.add.sprite(this.game.world.centerX,this.game.world.centerY, 'chicken');
+        this.chicken.anchor.setTo(0.5);
+
+        this.horse = this.game.add.sprite(120,10, 'horse');
+
+        this.pig = this.game.add.sprite(500,300, 'pig');
+        this.pig.anchor.setTo(0.5);
     },
     update: function(){
+
+
 
     },
 };
@@ -14,3 +28,5 @@ var game = new Phaser.game(640,360, Phaser.AUTO);
 
 game.state.add('GameState', GameState);
 game.state.start('GameState');
+
+// 0:54
